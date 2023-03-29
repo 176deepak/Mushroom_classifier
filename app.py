2da06c1e4a8b4f5268544ad0de4b4cf79a98b60e
Deepak <deepaksainiofficial17@gmail.com>
2023-03-29 20:03:40 +0530

    New files added

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
 from sklearn.preprocessing import OrdinalEncoder
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
 
 app = Flask(__name__)
 
@@ -11,36 +13,42 @@ def home():
 
 @app.route("/mushroom_classifier", methods=['GET', 'POST'])
 def classifier():
+    prediction = None
     if request.method=='GET':
         return render_template("classifier.html")
     else:
-        prediction = None
-        capShape = request.form["shape"]          
-        capSurface = request.form["surface"]            
-        capColor = request.form["color"]
-        bruises = request.form["bruises"]      
-        odor = request.form["odor"]                   
-        gillAttachment = request.form["attachment"]        
-        gillSpacing = request.form["spacing"]           
-        gillSize = request.form["size"]             
-        gillColor = request.form["gill-color"]             
-        stalkShape = request.form["stalk-shape"]            
-        stalkRoot = request.form["root"]            
-        stalkSurfaceAboveRing = request.form["ssa-ring"]
-        stalkSurfaceBelowRing = request.form["ssb-ring"]
-        stalkColorAboveRing = request.form["sca-ring"]
-        stalkColorBelowRing = request.form["scb-ring"]
-        veilType = request.form["veil-type"]             
-        veilColor = request.form["veil-color"]            
-        ringNumber = request.form["ring-number"]             
-        ringType = request.form["ring-type"]            
-        sporePrintColor = request.form["sp-color"]      
-        population = request.form["population"]          
-        habitat = request.form["habitat"]
+        data = CustomData(
+            capShape = request.form["shape"],         
+            capSurface = request.form["surface"],            
+            capColor = request.form["color"],
+            bruises = request.form["bruises"],      
+            odor = request.form["odor"],            
+            gillAttachment = request.form["attachment"],        
+            gillSpacing = request.form["spacing"],           
+            gillSize = request.form["size"],             
+            gillColor = request.form["gill-color"],             
+            stalkShape = request.form["stalk-shape"],            
+            stalkRoot = request.form["root"],            
+            stalkSurfaceAboveRing = request.form["ssa-ring"],
+            stalkSurfaceBelowRing = request.form["ssb-ring"],
+            stalkColorAboveRing = request.form["sca-ring"],
+            stalkColorBelowRing = request.form["scb-ring"],
+            veilType = request.form["veil-type"],             
+            veilColor = request.form["veil-color"],            
+            ringNumber = request.form["ring-number"],             
+            ringType = request.form["ring-type"],            
+            sporePrintColor = request.form["sp-color"],      
+            population = request.form["population"],          
+            habitat = request.form["habitat"]
+        )
+
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+        
 
 @app.route("/prediction")
 def pred():
     return render_template('prediction.html')
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run()
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -24,41 +24,79 @@ class PredictPipeline:
             raise CustomException(e,sys)
 
 
-
 class CustomData:
-    def __init__(  self,
-        gender: str,
-        race_ethnicity: str,
-        parental_level_of_education,
-        lunch: str,
-        test_preparation_course: str,
-        reading_score: int,
-        writing_score: int):
+    def __init__(self,
+                capShape:str,
+                capSurface:str,
+                capColor:str,
+                bruises:str,
+                odor:str,
+                gillAttachment:str,
+                gillSpacing,
+                gillSize:str,
+                gillColor:str,
+                stalkShape:str,
+                stalkRoot:str,
+                stalkSurfaceAboveRing:str,
+                stalkSurfaceBelowRing:str,
+                stalkColorAboveRing:str,
+                stalkColorBelowRing:str,
+                veilType:str,
+                veilColor:str,
+                ringNumber:str,
+                ringType,
+                sporePrintColor:str,
+                population:str,
+                habitat:str):
 
-        self.gender = gender
-
-        self.race_ethnicity = race_ethnicity
-
-        self.parental_level_of_education = parental_level_of_education
-
-        self.lunch = lunch
-
-        self.test_preparation_course = test_preparation_course
-
-        self.reading_score = reading_score
-
-        self.writing_score = writing_score
+        self.capShape = capShape
+        self.capSurface = capSurface
+        self.capColor = capColor
+        self.bruises = bruises
+        self.odor = odor
+        self.gillAttachment = gillAttachment
+        self.gillSpacing = gillSpacing
+        self.gillSize = gillSize
+        self.gillColor = gillColor
+        self.stalkShape = stalkShape
+        self.stalkRoot = stalkRoot
+        self.stalkSurfaceAboveRing = stalkSurfaceAboveRing
+        self.stalkSurfaceBelowRing = stalkSurfaceBelowRing
+        self.stalkColorAboveRing = stalkColorAboveRing
+        self.stalkColorBelowRing = stalkColorBelowRing
+        self.veilType = veilType
+        self.veilColor = veilColor
+        self.ringNumber = ringNumber
+        self.ringType = ringType
+        self.sporePrintColor = sporePrintColor
+        self.population = population
+        self.habitat = habitat
 
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
-                "gender": [self.gender],
-                "race_ethnicity": [self.race_ethnicity],
-                "parental_level_of_education": [self.parental_level_of_education],
-                "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_course],
-                "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score],
+                "capShape":[self.capShape], 
+                "capSurface":[self.capSurface], 
+                "capColor":[self.capColor], 
+                "bruises":[self.bruises],
+                "odor":[self.odor], 
+                "gillAttachment":[self.gillAttachment],
+                "gillSpacing":[self.gillSpacing],
+                "gillSize":[self.gillSize], 
+                "gillColor":[self.gillColor],
+                "stalkShape":[self.stalkShape], 
+                "stalkRoot":[self.stalkRoot],
+                "stalkSurfaceAboveRing":[self.stalkSurfaceAboveRing], 
+                "stalkSurfaceBelowRing":[self.stalkSurfaceBelowRing], 
+                "stalkColorAboveRing":[self.stalkColorAboveRing],
+                "stalkColorBelowRing":[self.stalkColorBelowRing], 
+                "veilType":[self.veilType], 
+                "veilColor":[self.veilColor],
+                "ringNumber":[self.ringNumber], 
+                "ringType":[self.ringType], 
+                "sporePrintColor":[self.sporePrintColor],
+                "population":[self.population], 
+                "habitat":[self.habitat]
             }
 
             return pd.DataFrame(custom_data_input_dict)
